0a6682d4dee6ed75073c253b76eb1666dab88008
Vinicius Coan <vinicp290907@gmail.com>
2025-04-04 11:08:58 -0300

    Iniciando o projeto

---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import { Text, StyleSheet } from 'react-native';
+
+const Title = () => {
+    return (
+        <Text style={style.title}>Calculadora de IMC</Text>
+    )
+}
+
+const style = StyleSheet.create({
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 24,
+    }
+})
+
+export default Title;
